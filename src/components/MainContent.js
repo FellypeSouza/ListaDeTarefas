6af1645dc47937c8b6d26c5fcc68b3a639850af4
FellypeSouza <fellypedev23@gmail.com>
2024-10-04 13:48:41 -0300

    modifying the project

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "../styles/MainContent.scss";
+import addIcon from "../images/addIcon.png";
+import emptyIcon from "../images/emptyIcon.png";
 
 export default function MainContent(props){
 
@@ -12,7 +14,14 @@ export default function MainContent(props){
     return(
         <section>
             <section className="tasks">
-                <h1>{tasks}</h1>
+                <section className="emptyTasks">
+                    <img src={emptyIcon} width={50} height={50} alt="Vazio"/>
+                    <h1>{tasks}</h1>
+                </section>
+                <section className="tasksBtn">
+                    <button id="addBtn">Adicionar Tarefa</button>
+                    <button id="delBtn">Excluir Tarefa</button>
+                </section>
             </section>
         </section>
     );
